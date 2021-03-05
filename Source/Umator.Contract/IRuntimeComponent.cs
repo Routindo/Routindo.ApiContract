5a72e6b1a6d33b134fc9ffb261be4221b174138d
Younes Cheikh <younes.cheikh@gmail.com>
2021-03-06 00:54:18 +0100

    Version 1.1.0 : Added Logging Service and Services Provider interfaces + Removed NLog reference

--- Source/Umator.Contract/IRuntimeComponent.cs
+++ Source/Umator.Contract/IRuntimeComponent.cs
@@ -1,7 +1,23 @@
-﻿namespace Umator.Contract
+﻿using Umator.Contract.Services;
+
+namespace Umator.Contract
 {
     public interface IRuntimeComponent
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logging service.
+        /// </summary>
+        /// <value>
+        /// The logging service.
+        /// </value>
+        ILoggingService LoggingService { get; set; }
     }
 }
---
+++ Source/Umator.Contract/Services/IServicesProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Umator.Contract.Services
+{
+    public interface IServicesProvider
+    {
+        ILoggingService GetLoggingService(string name, Type type = null);
+    }
+
+    public class ServicesContainer 
+    {
+        public static IServicesProvider ServicesProvider { get; internal set; }
+
+        private static readonly ServicesContainer Instance;
+
+        private static readonly object SyncObj = new object();
+
+        private ServicesContainer()
+        {
+
+        }
+
+        static ServicesContainer()
+        {
+            if(Instance != null)
+                return;
+
+            lock (SyncObj)
+            {
+                if(Instance != null)
+                    return;
+
+                Instance = new ServicesContainer();
+            }
+        }
+    }
+}
