3728676e613de5506d5807469a90ef6c4d64a92f
Younes Cheikh <younes.cheikh@gmail.com>
2021-03-06 23:50:30 +0100

    Version 1.1.1 Allow Setting ServicesProvider from ServicesContainer

--- Source/Umator.Contract/Services/IServicesProvider.cs
+++ Source/Umator.Contract/Services/IServicesProvider.cs
@@ -6,32 +6,4 @@ namespace Umator.Contract.Services
     {
         ILoggingService GetLoggingService(string name, Type type = null);
     }
-
-    public class ServicesContainer 
-    {
-        public static IServicesProvider ServicesProvider { get; internal set; }
-
-        private static readonly ServicesContainer Instance;
-
-        private static readonly object SyncObj = new object();
-
-        private ServicesContainer()
-        {
-
-        }
-
-        static ServicesContainer()
-        {
-            if(Instance != null)
-                return;
-
-            lock (SyncObj)
-            {
-                if(Instance != null)
-                    return;
-
-                Instance = new ServicesContainer();
-            }
-        }
-    }
 }
---
+++ Source/Umator.Contract/Services/ServicesContainer.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Umator.Contract.Services
+{
+    public class ServicesContainer 
+    {
+        public static IServicesProvider ServicesProvider { get; internal set; }
+
+        private static readonly ServicesContainer Instance;
+
+        private static readonly object SyncObj = new object();
+
+        private ServicesContainer()
+        {
+
+        }
+
+        public static void SetServicesProvider(IServicesProvider servicesProvider)
+        {
+            ServicesProvider = servicesProvider;
+        }
+
+        static ServicesContainer()
+        {
+            if(Instance != null)
+                return;
+
+            lock (SyncObj)
+            {
+                if(Instance != null)
+                    return;
+
+                Instance = new ServicesContainer();
+            }
+        }
+    }
+}
