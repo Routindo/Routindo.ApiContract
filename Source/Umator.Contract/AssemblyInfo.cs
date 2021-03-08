c7766b62d1c45c0bbc9a456de80bd2899b0eae05
Younes Cheikh <younes.cheikh@gmail.com>
2021-03-08 21:58:35 +0100

    Created Environment service -

--- Source/Umator.Contract/AssemblyInfo.cs
+++ Source/Umator.Contract/AssemblyInfo.cs
@@ -5,3 +5,10 @@
 "b53e58c2bc84393fd61c573bb10f9db7d3030990953b64c06dcdae01d82f327c54987a6871ad57"+
 "5b315d714f23c11dab781bb36d182b9cb05888d01678bbb1165546fd3f9fde00edb56fef566831"+
 "78d1fdaf")]
+
+[assembly: InternalsVisibleTo("Umator.Framework, PublicKey=" +
+                              "0024000004800000940000000602000000240000525341310004000001000100cd4d3b9bedf0a1" +
+                              "12a1372f86904047a47c4a9f06e05000cb465f206a1e812b990b831e2d7eb402ff9c05cd02d9e7" +
+                              "dff56ba8019f67b9921850c9b11891bb4c77f2bcfd3ac08b69d31dc3240a69b41344716da0ce56" +
+                              "696563932a2d79167b5877b6e7a41bfb947680250bdcebb4f3d1b52a85bcd443cb61110dcfc10c" +
+                              "9662d9c6")]
--- Source/Umator.Contract/Services/IServicesProvider.cs
+++ Source/Umator.Contract/Services/IServicesProvider.cs
@@ -5,5 +5,7 @@ namespace Umator.Contract.Services
     public interface IServicesProvider
     {
         ILoggingService GetLoggingService(string name, Type type = null);
+
+        IEnvironmentService GetEnvironmentService();
     }
 }
