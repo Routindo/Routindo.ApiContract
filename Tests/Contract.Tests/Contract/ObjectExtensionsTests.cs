8dbbc5172ca432b0c8848771b4216159cfa7038b
Younes Cheikh <younes.cheikh@gmail.com>
2021-04-01 19:18:03 +0200

    Test Object Extensions - Closes AB#18

---
+++ Tests/Contract.Tests/Contract/ObjectExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Routindo.Contract.Helpers;
+
+namespace Routindo.Contract.Tests.Contract
+{
+    [TestClass]
+    public class ObjectExtensionsTests
+    {
+        [TestMethod]
+        public void CheckIfNullableTest()
+        {
+            int? nullableNumber = null;
+            int nonNullableNumber = 1;
+
+            Assert.IsTrue(nullableNumber.IsNullable<int>());
+            Assert.IsFalse(nonNullableNumber.IsNullable<int>());
+        }
+    }
+}
