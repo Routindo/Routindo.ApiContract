3dc3ebf08e82e771de66c7c0518e6dcfa0deb4e2
Younes Cheikh <younes.cheikh@gmail.com>
2021-04-01 19:16:29 +0200

    Tested PluginUtilities Fixes AB#22

---
+++ Tests/Contract.Tests/Contract/UtilitiesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Routindo.Contract.Tests.Contract
+{
+    [TestClass]
+    public class UtilitiesTests
+    {
+        [TestMethod]
+        public void GetUniqueIdTest()
+        {
+            var uniqueId = PluginUtilities.GetUniqueId();
+            var upperCaseUniqueId = uniqueId.ToUpper();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(uniqueId));
+            // Assert unique Id is generated in upper cases
+            Assert.AreEqual(uniqueId, upperCaseUniqueId);
+        }
+    }
+}
