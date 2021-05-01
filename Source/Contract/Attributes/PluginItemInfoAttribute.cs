b14d03a99994d317313ee8dbb8d83adfbfcde0f9
Younes CHEIKH <younes.cheikh@gmail.com>
2021-05-01 02:26:40 +0200

    Added Category and FriendlyName for plugun item info

--- Source/Contract/Attributes/PluginItemInfoAttribute.cs
+++ Source/Contract/Attributes/PluginItemInfoAttribute.cs
@@ -5,13 +5,27 @@ namespace Routindo.Contract.Attributes
     [AttributeUsage(AttributeTargets.Class)]
     public class PluginItemInfoAttribute : Attribute
     {
+        private string _friendlyName;
         public string Id { get; set; }
 
         public string Name { get; }
 
         public string Description { get; }
 
-        public PluginItemInfoAttribute(string id, string name, string description)
+        public string Category { get; set; } = "Uncategorized";
+
+        public string FriendlyName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_friendlyName))
+                    return Name;
+                return _friendlyName;
+            }
+            set => _friendlyName = value;
+        }
+
+        public PluginItemInfoAttribute(string id, string name, string description = "")
         {
             Id = id;
             Name = name;
