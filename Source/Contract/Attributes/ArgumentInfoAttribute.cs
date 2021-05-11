796a890f1787e221675a036276d7163f09e88255
Younes CHEIKH <younes.cheikh@gmail.com>
2021-05-12 00:57:43 +0200

    Created ArgumentInfoAttribute for Execution and Results components arguments

---
+++ Source/Contract/Attributes/ArgumentInfoAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Routindo.Contract.Attributes
+{
+    public class ArgumentInfoAttribute: Attribute
+    {
+        public string DisplayName { get; }
+        public bool Mandatory { get; }
+        public Type Type { get; }
+        public string Description { get; }
+
+        public ArgumentInfoAttribute(string displayName, bool mandatory, Type type, string description = "")
+        {
+            DisplayName = displayName;
+            Mandatory = mandatory;
+            Type = type;
+            Description = description;
+        }
+    }
+}
