c38121accd69e8bc6a3ef228a2666d53937b88bc
Younes CHEIKH <younes.cheikh@gmail.com>
2021-05-12 05:29:06 +0200

    Added Argument Info attribute

--- Source/Contract/Attributes/ArgumentInfoAttribute.cs
+++ Source/Contract/Attributes/ArgumentInfoAttribute.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace Routindo.Contract.Attributes
 {
@@ -6,15 +8,34 @@ namespace Routindo.Contract.Attributes
     {
         public string DisplayName { get; }
         public bool Mandatory { get; }
-        public Type Type { get; }
+        public string Type { get; }
         public string Description { get; }
 
         public ArgumentInfoAttribute(string displayName, bool mandatory, Type type, string description = "")
         {
             DisplayName = displayName;
             Mandatory = mandatory;
-            Type = type;
+            Type = GetFullName(type);
             Description = description;
+        }
+
+        private string GetFullName(Type t)
+        {
+            if (!t.IsGenericType)
+                return t.Name;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(t.Name.Substring(0, t.Name.LastIndexOf("`", StringComparison.Ordinal)));
+            sb.Append(t.GetGenericArguments().Aggregate("<",
+
+                delegate (string aggregate, Type type)
+                {
+                    return aggregate + (aggregate == "<" ? "" : ",") + GetFullName(type);
+                }
+            ));
+            sb.Append(">");
+
+            return sb.ToString();
         }
     }
 }
---
+++ Tests/Contract.Tests/Contract/ArgumentInfoAttributeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Routindo.Contract.Attributes;
+
+namespace Routindo.Contract.Tests.Contract
+{
+    [TestClass]
+    public class ArgumentInfoAttributeTests
+    {
+        [TestMethod]
+        [TestCategory("Unit Test")]
+        public void SetValuesTest()
+        {
+            ArgumentInfoAttribute attribute = new ArgumentInfoAttribute("Fake", true, typeof(string), "Just another fake attribute");
+            Assert.AreEqual(nameof(String), attribute.Type);
+
+            attribute = new ArgumentInfoAttribute("Fake", true, typeof(String), "Just another fake attribute");
+            Assert.AreEqual(nameof(String), attribute.Type);
+
+            attribute = new ArgumentInfoAttribute("Fake", true, typeof(long), "Just another fake attribute");
+            Assert.AreEqual(nameof(Int64), attribute.Type);
+
+            attribute = new ArgumentInfoAttribute("Fake", true, typeof(long?), "Just another fake attribute");
+            Assert.AreEqual("Nullable<Int64>", attribute.Type);
+
+            attribute = new ArgumentInfoAttribute("Fake", true, typeof(ArgumentInfoAttributeTests), "Just another fake attribute");
+            Assert.AreEqual(nameof(ArgumentInfoAttributeTests), attribute.Type);
+
+            attribute = new ArgumentInfoAttribute("Fake", true, typeof(Nullable<>), "Just another fake attribute");
+            Assert.AreEqual("Nullable<T>", attribute.Type);
+        }
+    }
+}
